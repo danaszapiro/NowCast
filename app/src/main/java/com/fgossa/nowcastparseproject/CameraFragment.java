feddb773d0198ba07d28645918a4558b08540d50
Eduardo Portet <eduardoportet@gmail.com>
2015-12-11 22:25:15 -0500

    Added front facing camera feature

--- app/src/main/java/com/fgossa/nowcastparseproject/CameraFragment.java
+++ app/src/main/java/com/fgossa/nowcastparseproject/CameraFragment.java
@@ -32,6 +32,7 @@ public class CameraFragment extends Fragment {
     public static final String TAG = "CameraFragment";
 
     private Camera camera;
+    private int current_camera_id;  // ADDED THIS
     private SurfaceView surfaceView;
     private ParseFile photoFile;
     private ImageButton photoButton;
@@ -44,7 +45,8 @@ public class CameraFragment extends Fragment {
 
         if (camera == null) {
             try {
-                camera = Camera.open();
+                current_camera_id = Camera.CameraInfo.CAMERA_FACING_BACK; // ADDED THIS
+                camera = Camera.open(current_camera_id); //ADDED ARGUMENT
                 photoButton.setEnabled(true);
             } catch (Exception e) {
                 Log.e(TAG, "No camera with exception: " + e.getMessage());
@@ -53,6 +55,39 @@ public class CameraFragment extends Fragment {
                         Toast.LENGTH_LONG).show();
             }
         }
+
+
+        // ADDING CODE HERE
+        photoButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (true) {
+                    camera.stopPreview();
+                }
+                //NB: if you don't release the current camera before switching, you app will crash
+                camera.release();
+
+                //swap the id of the camera to be used
+                if(current_camera_id == Camera.CameraInfo.CAMERA_FACING_BACK){
+                    current_camera_id = Camera.CameraInfo.CAMERA_FACING_FRONT;
+                }
+                else {
+                    current_camera_id = Camera.CameraInfo.CAMERA_FACING_BACK;
+                }
+                camera = Camera.open(current_camera_id);
+                //Code snippet for this method from somewhere on android developers, i forget where
+                camera.setDisplayOrientation(90);
+                try {
+                    //this step is critical or preview on new camera will no know where to render to
+                    camera.setPreviewDisplay(surfaceView.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                camera.startPreview();
+                return false;
+            }
+        });
+        // END
 
         photoButton.setOnClickListener(new View.OnClickListener() {
 
@@ -107,7 +142,10 @@ public class CameraFragment extends Fragment {
 
         Bitmap momentImage = BitmapFactory.decodeByteArray(data, 0, data.length);
         Matrix matrix = new Matrix();
-        matrix.postRotate(90);
+        if (current_camera_id == Camera.CameraInfo.CAMERA_FACING_BACK)
+            matrix.postRotate(90);
+        else
+            matrix.postRotate(270);
         Bitmap rotatedScaledMomentImage = Bitmap.createBitmap(momentImage, 0, 0, momentImage.getWidth(), momentImage.getHeight(), matrix, true);
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         rotatedScaledMomentImage.compress(Bitmap.CompressFormat.JPEG, 100, bos);
@@ -134,7 +172,7 @@ public class CameraFragment extends Fragment {
         super.onResume();
         if (camera == null) {
             try {
-                camera = Camera.open();
+                camera = Camera.open(current_camera_id);
                 photoButton.setEnabled(true);
             } catch (Exception e) {
                 Log.i(TAG, "No camera: " + e.getMessage());
